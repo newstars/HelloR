da0160d09a323cb3671ccc2e47014e546a19a503
JunHee Kang <newstars77@gmail.com>
2019-01-06 22:29:29 +0900

    ggmap 사용법 변경
    ggmap 패키지 설치 방법 변경
    registrer_google() 함수 추가

--- Code/Chapter10/ggmap.R
+++ Code/Chapter10/ggmap.R
@@ -1,9 +1,20 @@
 # 10장 ggmap
+# Google Map Platform 출시로 인해 ggmap 패키지 변경됨
 
-install.packages("ggmap")
+install.packages("devtools")
+library(devtools)
+install_github("dkahle/ggmap")
 library(ggmap)
 
+# http://developers.google.com/maps/terms
+# 1. 상기 주소에서 구글 계정 인증 필요
+# 2. 프로젝트 신규로 생성하기
+# 3. Google Maps Platform API Key 활용하기
+
+googleAPIkey = "API 키 입력하기"
+
 # seoul의 위치 정보를 가져온 후 gg_seoul 변수에 할당
+register_google(googleAPIkey)
 gg_seoul <- get_googlemap("seoul", maptype = "roadmap")
 ggmap(gg_seoul)                           # gg_seoul의 위치 값에 따른 구글 지도 호출
 
@@ -19,5 +30,3 @@ geo_data <- as.numeric(geo_code)              # 리스트를 숫자로 변환하
 get_googlemap(center=geo_data, maptype="roadmap", zoom=13) %>% ggmap()+
   # geo_code에 있는 경도(lon)와 위도(lat) 값으로 산점도 그리기
   geom_point(data=geo_code, aes(x=geo_code$lon, y=geo_code$lat))    
-
-
